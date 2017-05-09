b66f7704d62804bfb6a38045efd23cc004e97fec
rikumax25 <jorgev259@outlook.es>
2017-05-08 18:08:19 -0600

    fixed port

--- segasaturn.js
+++ segasaturn.js
@@ -105,7 +105,7 @@ function cleanup(){
      res.sendFile( __dirname + req.params[0]); 
  });
 
- var port = process.env.PORT || 5000;
+ var port = process.env.PORT || 8080;
  app.listen(port, function() {
    console.log("Listening on " + port);
  });
